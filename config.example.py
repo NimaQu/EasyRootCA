ee68bd2d9d0f5ee75678a694690cd63341077f92
NimaQu <i@nimaqu.com>
2021-07-05 20:27:45 +0800

    从 config 读取配置，将第一个输入的域名或者 ip 作为证书 CN

---
+++ config.example.py
@@ -0,0 +1,5 @@
+# cert information
+COUNTRY_NAME = "CA"
+STATE_OR_PROVINCE_NAME = "Ontario"
+LOCALITY_NAME = "Toronto"
+ORGANIZATION_NAME = "NimaQu Root CA"
--- main.py
+++ main.py
@@ -8,21 +8,27 @@ from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 from ipaddress import IPv4Address, IPv6Address
+import config
 
 
 def load_ca_file(file_type):
-    if file_type == 'cert':
-        with open("rootCA_cert.pem", "rb") as f:
-            return x509.load_pem_x509_certificate(f.read())
-    if file_type == 'key':
-        with open("rootCA_key.pem", "rb") as f:
-            return serialization.load_pem_private_key(f.read(), None)
-    else:
-        print("Cant find CA File")
-        return None
+    try:
+        if file_type == 'cert':
+            with open("rootCA_cert.crt", "rb") as f:
+                return x509.load_pem_x509_certificate(f.read())
+    except FileNotFoundError:
+        print("Can't find root cert File")
+        exit(114514)
+    try:
+        if file_type == 'key':
+            with open("rootCA_key.pem", "rb") as f:
+                return serialization.load_pem_private_key(f.read(), None)
+    except FileNotFoundError:
+        print("Can't find root key file")
+        exit(114514)
 
 
-def generate_csr():
+def generate_csr(common_name):
     key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
@@ -37,17 +43,16 @@ def generate_csr():
 
     csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
         # Provide various details about who we are.
-        x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"My Company"),
-        x509.NameAttribute(NameOID.COMMON_NAME, u"mysite.com"),
+        x509.NameAttribute(NameOID.COUNTRY_NAME, config.COUNTRY_NAME),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, config.STATE_OR_PROVINCE_NAME),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, config.LOCALITY_NAME),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, config.ORGANIZATION_NAME),
+        x509.NameAttribute(NameOID.COMMON_NAME, common_name),
     ])).sign(key, hashes.SHA256())
-    # Write our CSR out to disk.
     return csr
 
 
-def sign_certificate_request(csr_cert, ca_cert, private_ca_key):
+def sign_certificate_request(csr_cert, ca_cert, private_ca_key, ext_data):
     cert = x509.CertificateBuilder().subject_name(
         csr_cert.subject
     ).issuer_name(
@@ -63,7 +68,7 @@ def sign_certificate_request(csr_cert, ca_cert, private_ca_key):
         datetime.datetime.utcnow() + datetime.timedelta(days=10)
         # Sign our certificate with our private key
     ).add_extension(
-        x509.SubjectAlternativeName(user_input_alt_name()),
+        x509.SubjectAlternativeName(ext_data),
         critical=False,
         # Sign the CSR with our private key.
     ).sign(private_ca_key, hashes.SHA256())
@@ -83,11 +88,18 @@ def check_is_ip(data):
 
 def user_input_alt_name():
     data = []
+    count = 0
+    common_name = ""
     print("Please enter the domain or ip address you want to create in the cert")
     print("Enter 1 domain or IP address each time, press ENTER to end")
+
     while True:
         input_data = ""
-        input_data = input("")
+        input_data = input()
+
+        if count == 0:
+            common_name = input_data
+            count = count + 1
         if input_data == "":
             break
         if check_is_ip(input_data):
@@ -99,13 +111,16 @@ def user_input_alt_name():
             data.append(x509.DNSName(input_data))
         print("current domain or ipaddress:", end="")
         print(data)
-    return data
+    return common_name, data
 
 
-# ca_cert = bytes(open("rootCA_cert.pem"))
-local_csr_cert = generate_csr()
-local_ca_cert = load_ca_file("cert")
-local_ca_key = load_ca_file("key")
+def main():
+    common_name, ext_data = user_input_alt_name()
+    local_csr_cert = generate_csr(common_name)
+    local_ca_cert = load_ca_file("cert")
+    local_ca_key = load_ca_file("key")
+    sign_certificate_request(local_csr_cert, local_ca_cert, local_ca_key, ext_data)
 
-sign_certificate_request(local_csr_cert, local_ca_cert, local_ca_key)
-# user_input_alt_name()
+
+if __name__ == "__main__":
+    main()
